56dd76e4e31dfdfdd665faa6c49dda28a9ede4ef
raaz4n <raazandoski@gmail.com>
2025-07-16 19:52:46 -0500

    start

---
+++ websiteCheck.py
@@ -0,0 +1,5 @@
+''' yagmail will transmit delivery to a specified email.
+    requests and BeautifulSoup will get information from
+    a URL and trip once a change occurs within the website.'''
+import yagmail, requests
+from bs4 import BeautifulSoup
