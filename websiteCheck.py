6aea24d907cc5b36692cecd4cb2858d9d2901c13
raaz4n <raazandoski@gmail.com>
2025-07-17 18:58:02 -0500

    smtplib addition

--- websiteCheck.py
+++ websiteCheck.py
@@ -1,5 +1,9 @@
-''' yagmail will transmit delivery to a specified email.
+# websiteCheck.py
+# developed by raaz4n@github
+''' smtplib will transmit delivery to a specified email.
     requests and BeautifulSoup will get information from
     a URL and trip once a change occurs within the website.'''
-import yagmail, requests
+
+import smtplib, requests
 from bs4 import BeautifulSoup
+
